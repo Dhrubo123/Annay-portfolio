dfd2edbca38ae6655e67dd57f0c7f3b508f1e4c6
Dhrubo123 <aparupdrubha3@gmail.com>
2026-04-30 00:13:20 +0600

    linkdin

--- src/app/page.js
+++ src/app/page.js
@@ -65,8 +65,8 @@ export default function Home() {
       <header className={`${styles.header} ${isScrolled ? styles.headerScrolled : ''}`}>
         <div className={`container ${styles.nav}`}>
           <h1 className={styles.logo}>AB.</h1>
-          
-          <button 
+
+          <button
             className={`${styles.mobileMenuBtn} ${isMenuOpen ? styles.menuActive : ''}`}
             onClick={() => setIsMenuOpen(!isMenuOpen)}
             aria-label="Toggle Menu"
@@ -87,7 +87,7 @@ export default function Home() {
               <a href="https://github.com/Annaybarua" target="_blank" aria-label="GitHub">
                 <svg viewBox="0 0 24 24"><path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.57 9.57 0 0112 6.836c.85.004 1.705.114 2.51.336 1.909-1.294 2.748-1.025 2.748-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482C19.138 20.161 22 16.418 22 12c0-5.523-4.477-10-10-10z" /></svg>
               </a>
-              <a href="https://www.linkedin.com/in/annay-barua-405849204?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" aria-label="LinkedIn">
+              <a href="https://www.linkedin.com/in/annay-barua-405849204/" target="_blank" aria-label="LinkedIn">
                 <svg viewBox="0 0 24 24"><path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a2.7 2.7 0 0 0-2.7-2.7c-.8 0-1.5.4-1.9 1v-1h-2.5v8h2.5v-4.3c0-.6.5-1.1 1.1-1.1.6 0 1.1.5 1.1 1.1v4.3h2.5M7 18.5V9h2.5v9.5H7M8.3 7.5c-.8 0-1.5-.7-1.5-1.5s.7-1.5 1.5-1.5 1.5.7 1.5 1.5-.7 1.5-1.5 1.5z" /></svg>
               </a>
             </div>
